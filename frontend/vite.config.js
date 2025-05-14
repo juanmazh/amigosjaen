d70718b40ae398c75a778f2735adbb63c742ee81
Juan Manuel Zafra Hernández <jzafher0401@g.educaand.es>
2025-05-14 15:32:11 +0200

    pruebas varias

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,4 +1,36 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    base: './', // Asegura que las rutas sean relativas
+    plugins: [
+      react(),
+      tailwindcss(),
+    ],
+    css: {
+      preprocessorOptions: {
+        css: {
+          charset: false, // Evita problemas con el tipo MIME
+        },
+      },
+    },
+    server: {
+      hmr: true, // Asegura que HMR esté habilitado
+    },
+    define: {
+      'process.env': env, // Cargar variables de entorno
+    },
+    build: {
+      outDir: 'dist', // Carpeta de salida
+    },
+  };
+});
+
+/**import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from "@tailwindcss/vite";
 
@@ -21,4 +53,4 @@ export default defineConfig(async () => ({
   build: {
     outDir: 'dist', // Carpeta de salida
   },
-}));
+})); */
