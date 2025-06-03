3fd214235bc2c8296b22805dc587eda77bd98e63
Juan Manuel Zafra Hernández <jzafher0401@g.educaand.es>
2025-06-03 22:13:10 +0200

    version 0.9.2

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -19,8 +19,8 @@ export const AuthProvider = ({ children }) => {
       }
 
       try {
-       const res = await axios.get('http://localhost:5000/api/auth/usuario', {
-      // const res = await axios.get('https://amigosjaen.onrender.com/api/auth/usuario', {
+      // const res = await axios.get('http://localhost:5000/api/auth/usuario', {
+       const res = await axios.get('https://amigosjaen.onrender.com/api/auth/usuario', {
           headers: {
             Authorization: `Bearer ${token}`,
           },
@@ -41,8 +41,8 @@ export const AuthProvider = ({ children }) => {
   }, [token]);
 
   const login = async (email, contraseña) => {
-     const res = await axios.post('http://localhost:5000/api/auth/login', {
-    //const res = await axios.post('https://amigosjaen.onrender.com/api/auth/login', {
+    //const res = await axios.post('http://localhost:5000/api/auth/login', {
+    const res = await axios.post('https://amigosjaen.onrender.com/api/auth/login', {
       email,
       contraseña,
     });
