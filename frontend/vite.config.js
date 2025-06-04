f0e07bafe5ec4a388a87abf0d9beff18c6311e55
Juan Manuel Zafra Hernández <jzafher0401@g.educaand.es>
2025-06-04 08:44:29 +0200

    fix router react 2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from "@tailwindcss/vite";
-import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
@@ -11,14 +10,6 @@ export default defineConfig(({ mode }) => {
     plugins: [
       react(),
       tailwindcss(),
-      viteStaticCopy({
-        targets: [
-          {
-            src: 'public/_redirects',
-            dest: '.'
-          }
-        ]
-      })
     ],
     css: {
       preprocessorOptions: {
