00dca2fb9aa7fc3cf806a076b0c25a4643ecf5b3
Juan Manuel Zafra Hernández <jzafher0401@g.educaand.es>
2025-06-09 11:20:20 +0200

    bugs de terminar

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -20,3 +20,22 @@ module.exports = (req, res, next) => {
     res.status(401).json({ msg: "Token inválido" });
   }
 };
+require('dotenv').config(); // importante
+
+module.exports = (req, res, next) => {
+  const authHeader = req.header("Authorization");
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ msg: "No hay token o formato incorrecto" });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.usuarioId = decoded.id;
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: "Token inválido" });
+  }
+};
